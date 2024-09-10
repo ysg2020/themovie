5de093122d9828a7fc681408cda1f918fb0c281d
YSG <dbtjrrbs12@naver.com>
2024-09-11 00:23:25 +0900

    Refactor : 반복되는 부분 리팩토링

--- src/main/java/com/ecocow/themovie/service/impl/MovieServiceImpl.java
+++ src/main/java/com/ecocow/themovie/service/impl/MovieServiceImpl.java
@@ -8,6 +8,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
@@ -23,63 +24,49 @@ public class MovieServiceImpl implements MovieService {
     @Value("${the-movie-api-key}")
     private String theMovieApiKey;
 
+    private String theMovieApiUrl = "https://api.themoviedb.org/3/movie/";
+
     @Override
     public MovieDetailsDto getMovie(Long movieId, String language) throws IOException, InterruptedException {
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("https://api.themoviedb.org/3/movie/"+movieId+"?language="+language))
-                .header("accept", "application/json")
-                .header("Authorization", theMovieApiKey)
-                .method("GET", HttpRequest.BodyPublishers.noBody())
-                .build();
-        HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
-        System.out.println(response.body());
-
-        return objectMapper.readValue(response.body(), MovieDetailsDto.class);
+        String uri = theMovieApiUrl + movieId + "?language=" + language;
+        return getTheMovieAPI(uri, MovieDetailsDto.class);
 
     }
 
+
     @Override
     public MovieResultDto getRecommendMovie(Long movieId, String language, int page) throws IOException, InterruptedException {
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("https://api.themoviedb.org/3/movie/"+movieId+"/recommendations?language="+language+"&page="+page))
-                .header("accept", "application/json")
-                .header("Authorization", theMovieApiKey)
-                .method("GET", HttpRequest.BodyPublishers.noBody())
-                .build();
-        HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
-        System.out.println(response.body());
-
-        return objectMapper.readValue(response.body(), MovieResultDto.class);
+        String uri = theMovieApiUrl + movieId + "/recommendations?language=" + language + "&page=" + page;
+        return getTheMovieAPI(uri,MovieResultDto.class);
 
     }
 
     @Override
     public MovieResultDto getPopularMovie(String language, int page) throws IOException, InterruptedException {
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("https://api.themoviedb.org/3/movie/popular?language="+language+"&page="+page))
-                .header("accept", "application/json")
-                .header("Authorization", theMovieApiKey)
-                .method("GET", HttpRequest.BodyPublishers.noBody())
-                .build();
-        HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
-        System.out.println(response.body());
-
-        return objectMapper.readValue(response.body(), MovieResultDto.class);
+        String uri = theMovieApiUrl + "popular?language=" + language + "&page=" + page;
+        return getTheMovieAPI(uri,MovieResultDto.class);
 
     }
 
 
     @Override
     public CreditsDto getCast(Long movieId, String language) throws IOException, InterruptedException {
+        String uri = theMovieApiUrl + movieId + "/credits?language=" + language;
+        return getTheMovieAPI(uri,CreditsDto.class);
+
+    }
+
+    private <T> T getTheMovieAPI(String uri,Class<T> responseType) throws IOException, InterruptedException {
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("https://api.themoviedb.org/3/movie/"+movieId+"/credits?language="+language))
+                .uri(URI.create(uri))
                 .header("accept", "application/json")
                 .header("Authorization", theMovieApiKey)
                 .method("GET", HttpRequest.BodyPublishers.noBody())
                 .build();
         HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
-        System.out.println(response.body());
+        return objectMapper.readValue(response.body(), responseType);
 
-        return objectMapper.readValue(response.body(), CreditsDto.class);
     }
+
+
 }
